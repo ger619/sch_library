7001e5888ea9c0326de5e6faec95f3274a9a1501
David Abol Ger <draker74@gmail.com>
2022-07-20 13:13:03 +0300

    Capitalise

--- person.rb
+++ person.rb
@@ -1,8 +1,11 @@
-class Person
+require_relative './nameable'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name: 'unknown', parent_permission: true)
+  def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @age = age
     @name = name
@@ -13,12 +16,13 @@ class Person
     @age > 18 || @parent_permission
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
-
-Person.new(12, name: 'Abol', parent_permission: false)
-Person.can_use_service?
---
+++ trimmer_decorator.rb
@@ -0,0 +1,10 @@
+require_relative './decorator'
+class TrimmerDecorator < Decorator
+  def correct_name
+    if @nameable.correct_name.length > 10
+      @nameable.correct_name.slice(0, 10).to_s
+    else
+      @nameable.correct_name
+    end
+  end
+end
